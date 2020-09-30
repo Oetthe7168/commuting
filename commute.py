526d9a71e8c9c62fb46bfa1fca6cd6cbc49f1aca
Oetthe7168 <72132485+Oetthe7168@users.noreply.github.com>
2020-09-30 19:52:11 +0900

    Update commute.py

--- commute.py
+++ commute.py
@@ -6,7 +6,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★표시될 게임이름★')
+    game = discord.Game('Oetthe Alts 하는중')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
@@ -18,10 +18,10 @@ async def on_message(message):
             if message.author.guild_permissions.manage_messages:
                 author = message.guild.get_member(int(message.author.id))
                 embed = discord.Embed(color=0x80E12A)
-                channel = ★전송될 채널 아이디★
+                channel =760802821082841118
                 embed.set_author(name=author, icon_url=message.author.avatar_url)
                 embed.add_field(name='관리자 출퇴근 알림', value='관리자가 출근하였습니다.')
-                embed.set_image(url="★이미지 url★")
+                embed.set_image(url="https://cdn.discordapp.com/attachments/756775123771457540/760816143853158430/1.png")
                 await client.get_channel(int(channel)).send(embed=embed)
         except:
             pass
@@ -31,12 +31,12 @@ async def on_message(message):
             if message.author.guild_permissions.manage_messages:
                 author = message.guild.get_member(int(message.author.id))
                 embed = discord.Embed(color=0xFF0000)
-                channel = ★전송될 채널 아이디★
+                channel =760802821082841118
                 embed.set_author(name=author, icon_url=message.author.avatar_url)
                 embed.add_field(name='관리자 출퇴근 알림', value='관리자가 퇴근하였습니다.')
-                embed.set_image(url="★이미지 url★")
+                embed.set_image(url="https://cdn.discordapp.com/attachments/756775123771457540/760816143853158430/1.png")
                 await client.get_channel(int(channel)).send(embed=embed)
         except:
             pass
 
-client.run('★봇 토큰★')
+client.run('NzYwODAyMjg5OTI1MDk1NDc1.X3RWWA.BxOyTM-ej9SCDVUgRNTlABN2SdE')
